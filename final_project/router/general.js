d7d39a29380e68fd224c3e7d109b7245950b0ee0
Gervais BERI <beriaxel@gmail.com>
2024-10-11 11:41:26 +0000

    Codes to get books by author and title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,25 +12,40 @@ public_users.post("/register", (req, res) => {
 // Get the book list available in the shop
 public_users.get("/", function (req, res) {
   //Write your code here
-  res.status(200).send(JSON.stringify(books));
+  res.status(200).json(books);
 });
 
 // Get book details based on ISBN
 public_users.get("/isbn/:isbn", function (req, res) {
   //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  let isbn = req.params.isbn;
+  let book = books[isbn];
+
+  if (!book) {
+    return res.status(404).json({ message: "Book not found" });
+  } else {
+    return res.status(200).json(book);
+  }
 });
 
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
   //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  let author = req.params.author;
+  let booksByAuthor = Object.values(books).filter(
+    (books) => books.author === author
+  );
+  return res.status(200).json(booksByAuthor);
 });
 
 // Get all books based on title
 public_users.get("/title/:title", function (req, res) {
   //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  let title = req.params.title;
+  let booksByTitle = Object.values(books).filter(
+    (books) => books.title === title
+  );
+  return res.status(200).json(booksByTitle);
 });
 
 //  Get book review
